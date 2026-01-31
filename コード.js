abbcc73b0126d9c5775f0bf6a83e3b3c7c153d88
KitauchiRyota <konbu2605@gmail.com>
2026-02-01 02:40:24 +0900

    フォルダからアイテム取得

--- コード.js
+++ コード.js
@@ -6,13 +6,35 @@ function doGet() {
       .setTitle('データ複製App'); // ブラウザのタブに表示されるタイトル
 }
 
+/**
+ * フォルダIDからファイルイテレータを取得する関数のテスト
+ */
+function getFolderItems(){
+  const srcFolderId = '17PRLe1GPz-6tFj9oZ1RgtN9uyCc15q-2'; // 1-3のフォルダ
+
+  const originfolder = DriveApp.getFolderById(srcFolderId);
+  const folders = originfolder.getFolders();
+  const files = originfolder.getFiles();
+
+  while (folders.hasNext()) {
+    const folder = folders.next();
+    Logger.log(folder.getName());
+  }
+
+  while (files.hasNext()) {
+    const file = files.next();
+    Logger.log(file.getName());
+  }
+}
+
 
 function copyFileByDriveApi() {
   // const srcFileId = '1-fpxrIkw_pUf8iUk-9Zc5lOPhUg1cr5k'; // .pptx
   // const srcFileId = '1WoK7GbhfnOgS1cV4FpQivGFielqRRJx5ekzo9elhGgGKx-9m1zUzYcoh'; // GAS
   // const srcFileId = '1AS_I_1iAoorcdUqDvEMr4UfrLlx1zkzJa_cXXvwaohU'; // spreadSheet
   // const srcFileId = '1PewdOTCoo99PfIa2oK9_PWQ9y-eGjeDU';  // 3-3のぱそぶー.pptx
-  const srcFileId = '1MrPtcqAqGmbt9rJiOxzA4_YFYEPqHcLpgArX7G5PB6_hOTpyEU-D3YC7'; // 1-3のGAS
+  // const srcFileId = '1MrPtcqAqGmbt9rJiOxzA4_YFYEPqHcLpgArX7G5PB6_hOTpyEU-D3YC7'; // 1-3のGAS
+  const srcFileId = '17Zdb_RgUSG0IRiOtV1MsC2abQ1TMZgdR'; // 1-3のショートカット
 
   // const destFolderId = '1FIFoJSRiYjX6RNb83H13eHJ4nomUchC0'; // 26Adv1st作成班
   const destFolderId = '1Nzfm_YXWyhjWEPImFWtM9-OWAIfZZreJ'; // PCSU_3-3
@@ -20,7 +42,7 @@ function copyFileByDriveApi() {
   const fileName = DriveApp.getFileById(srcFileId).getName();
   Logger.log(fileName);
 
-// ショートカットなど、全ての形式を統一処理でコピーするため、makeCopyでは無く　APIをGASから呼び出す
+  // ショートカットなど、全ての形式を統一処理でコピーするため、makeCopyでは無く　APIをGASから呼び出す
   const copied = Drive.Files.copy(
     {
       title: namedest,
@@ -45,13 +67,4 @@ function copyFileByDriveApi() {
   }
 
   Logger.log(copied.id);
-}
-
-/**
- * HTMLから呼び出されるGAS関数（例：メッセージを返す）
- * @param {string} name HTMLから渡される引数
- * @returns {string} GASからHTMLに返すメッセージ
- */
-function getGreetingMessage(name) {
-  return 'こんにちは、' + name + 'さん！GASからのメッセージです。';
 }
